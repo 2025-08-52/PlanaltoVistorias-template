c04c6c03161f66c8d24725bcb9ae10c380384efd
geovan-na <geovannarezendedossantos@gmail.com>
2025-09-30 21:52:06 -0300

    Update Vistoria.java

--- DF_Vistoria/src/Vistoria/model/Vistoria.java
+++ DF_Vistoria/src/Vistoria/model/Vistoria.java
@@ -1,1 +1,76 @@
+package Vistoria.model;
+
+import java.time.LocalDate;
+
+public class vistoria{
+	private int idVistoria;
+	private int idFuncionarios;
+	private int idAgendamento;
+	private LocalDate dataVistoria;
+	private String itensVerificados;
+	private String observacao;
+}
+public vistoria() {
+}
+public vistoria(int idVistoria, int idFuncionarios, int idAgendamento,LocalDate dataVistoria, String itensVerificados, String observacao) {
+	super();
+	this.idVistoria = idVistoria;
+	this.idFuncionarios = idFuncionarios;
+	this.idAgendamento = idAgendamento;
+	this.dataVistoria = dataVistoria;
+	this.itensVerificados  = itensVerificados;
+	this.observacao = observacao;
+}
+
+//get's
+public int getIdVistoria() {
+    return idVistoria;
+}
+
+public int getIdFuncionarios() {
+    return idFuncionarios;
+}
+
+public int getIdAgendamento() {
+    return idAgendamento;
+}
+
+public LocalDate getDataVistoria() {
+    return dataVistoria;
+}
+
+public String getItensVerificados() {
+    return itensVerificados;
+}
+
+public String getObservacao() {
+    return observacao;
+}
+
+//set's
+
+public void setIdVistoria(int idVistoria) {
+    this.idVistoria = idVistoria;
+}
+
+public void setIdFuncionarios(int idFuncionarios) {
+    this.idFuncionarios = idFuncionarios;
+}
+
+public void setIdAgendamento(int idAgendamento) {
+    this.idAgendamento = idAgendamento;
+}
+
+public void setDataVistoria(LocalDate dataVistoria) {
+    this.dataVistoria = dataVistoria;
+}
+
+public void setItensVerificados(String itensVerificados) {
+    this.itensVerificados = itensVerificados;
+}
+
+public void setObservacao(String observacao) {
+    this.observacao = observacao;
+}
+}
 
