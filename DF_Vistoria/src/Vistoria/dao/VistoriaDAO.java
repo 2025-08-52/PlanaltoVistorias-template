e3d88067214f51f79bf5f0c50960e4d843aa9e0e
geovan-na <geovannarezendedossantos@gmail.com>
2025-10-02 21:34:18 -0300

    Update VistoriaDAO.java

--- DF_Vistoria/src/Vistoria/dao/VistoriaDAO.java
+++ DF_Vistoria/src/Vistoria/dao/VistoriaDAO.java
@@ -8,17 +8,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
-
+import java.sql.Date;
 public class VistoriaDAO {
 	
 	public boolean inserirVistoria(Vistoria vistoria) {
-		 String sql = "INSERT INTO vistoria(Data_Vistoria, Itens_Verificados, Observacao) VALUES (?, ?, ?, ?, ?, ?)";
+		 String sql = "INSERT INTO vistoria(Id_Funcionario, Id_Agendamento, Data_Vistoria, Itens_Verificados, Observacao) VALUES (?, ?, ?, ?, ?, ?)";
 		 try (Connection conn = Conexao.getConnection();
 				 PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
 			 stmt.setInt(1, vistoria.getFuncionario().getId_Funcionario());
-			 stmt.setDate(1, vistoria.getDataVistoria());
-			 stmt.setString(2, vistoria.getItensVerificados());
-			 stmt.setString(3, vistoria.getObservacao());
+			 stmt.setInt(2, vistoria.getAgendamento().getId());
+			 stmt.setDate(3, vistoria.getDataVistoria());
+			 stmt.setString(4, vistoria.getItensVerificados());
+			 stmt.setString(5, vistoria.getObservacao());
 			 int rowsAffected = stmt.executeUpdate();
 			 
 			 vistoria.setDataVistoria(Date.valueOf("2025-09-12"));
